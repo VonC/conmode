93fe7f1782842c0bb09d5485d2e1a218bd7abf3a
VonC <vonc@laposte.net>
2022-02-24 00:36:18 +0100

    senv.example.bat: set GO variables examples

--- senv.example.bat
+++ senv.example.bat
@@ -1,2 +1,6 @@
 set "var=value"
 set "LINUX_ACCOUNT=remote linux machine where to copy linux-compiled executable"
+
+set GO111MODULE=on
+set GOPROXY=https://proxy.golang.org
+set GOROOT=C:\path\to\gos\current
